5ff65c5c221984aa04c8e8e044e194c8f78cc5f0
jeremiegrodziski <jeremie.github@grodziski.com>
2019-02-22 14:34:06 +0100

    Enhance release script

--- release.sh
+++ release.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 RELEASE_LEVEL=$1
+echo "Release \"keycloak-clojure\" with level $RELEASE_LEVEL"
 tag=$(clj -Arelease $RELEASE_LEVEL)
 
 if [ $? -eq 0 ]; then
-    echo "Successfully release \"keycloak-clojure\" tag $tag"
+    echo "Successfully released \"keycloak-clojure\" to $tag"
 else
     echo "Fail to release \"keycloak-clojure\"!"
     exit 1
@@ -14,11 +15,12 @@ if [[ $tag =~ v(.+) ]]; then
     newversion=${BASH_REMATCH[1]}
 else
     echo "unable to parse tag $tag"
+    exit 1
 fi
 mvn versions:set -DnewVersion=$newversion  2>&1 > /dev/null
 
 if [ $? -eq 0 ]; then
-    echo "Successfully set new version of \"keycloak-clojure\"'s pom to $nreversion"
+    echo "Successfully set new version of \"keycloak-clojure\"'s pom to $newversion"
 else
     echo "Fail to set new version of \"keycloak-clojure\"'s pom!"
     exit 1
@@ -26,7 +28,7 @@ fi
 
 mvn deploy  2>&1 > /dev/null
 if [ $? -eq 0 ]; then
-    echo "Successfully deploy \"keycloak-clojure\" version $newversion to clojars"
+    echo "Successfully deployed \"keycloak-clojure\" version $newversion to clojars"
 else
     echo "Fail to deploy \"keycloak-clojure\" to clojars!"
     exit 1
