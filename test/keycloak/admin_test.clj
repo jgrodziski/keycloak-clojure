2f26fd1d55372af406728625fe70c35a90373585
jeremiegrodziski <jeremie.github@grodziski.com>
2019-06-20 15:41:36 +0100

    Fix tests

--- test/keycloak/admin_test.clj
+++ test/keycloak/admin_test.clj
@@ -9,10 +9,7 @@
 (def integration-test-conf
   (client-conf "master" "admin-cli" "http://localhost:8090/auth"))
 
-(comment (def kc-admin4 (keycloak-client (client-conf "master" "mybackend" "http://localhost:8080/auth" nil) "10337ce2-8dee-44a9-80b2-bc08ff8fc695"))
-        (create-realm! kc-admin3 "test11" "base"))
-
-(deftest admin-test
+(deftest ^:integration admin-test
   (let [admin-client (keycloak-client integration-test-conf admin-login admin-password)]
     (testing "realm creation then deletion"
       (let [realm-name (str "keycloak-clojure-test" (rand-int 1000))
--- test/keycloak/authz_test.clj
+++ test/keycloak/authz_test.clj
@@ -1,16 +1,23 @@
 (ns keycloak.authz-test
   (:require [keycloak.admin :refer :all]
-            [keycloak.deployment :as deploy]
+            [keycloak.deployment :as deploy :refer [client-conf keycloak-client]]
             [keycloak.authz :as authz]
             [clojure.test :as t]))
 
-(def kc-admin (deploy/keycloak-client
-               (deploy/client-conf "master" "mybackend" "http://localhost:8080/auth")
-               "10337ce2-8dee-44a9-80b2-bc08ff8fc695"))
+(def admin-login "admin")
+(def admin-password "secretadmin")
+(def integration-test-conf
+  (client-conf "master" "admin-cli" "http://localhost:8090/auth"))
 
+(def admin-client (deploy/keycloak-client integration-test-conf admin-login admin-password))
 
-(def kc-authz (authz/authz-client (deploy/client-conf-input-stream "master" "mybackend" "http://localhost:8080/auth" "10337ce2-8dee-44a9-80b2-bc08ff8fc695")))
+(comment 
+  (def kc-admin (deploy/keycloak-client
+                 (deploy/client-conf "master" "mybackend" "http://localhost:8080/auth")
+                 "10337ce2-8dee-44a9-80b2-bc08ff8fc695"))
 
-(authz/create-resource kc-authz "My resource" "urn:my-authz:resources:my-resource" ["urn:my-authz:scopes:access"])
+  (def kc-authz (authz/authz-client (deploy/client-conf-input-stream "master" "mybackend" "http://localhost:8080/auth" "10337ce2-8dee-44a9-80b2-bc08ff8fc695")))
 
-(authz/create-role-policy kc-admin "master" "mybackend" "testrole" "urn:my-authz:resources:my-resource" ["urn:my-authz:scopes:access"] )
+  (authz/create-resource kc-authz "My resource" "urn:my-authz:resources:my-resource" ["urn:my-authz:scopes:access"])
+
+  (authz/create-role-policy kc-admin "master" "mybackend" "testrole" "urn:my-authz:resources:my-resource" ["urn:my-authz:scopes:access"] ))
