52ae1d1b091c882768e5298acdd806e4198f6f71
jeremiegrodziski <jeremie@grodziski.com>
2020-11-09 10:51:25 +0100

    Fix build and release script

--- release.sh
+++ release.sh
@@ -21,7 +21,10 @@ fi
 #     Clojars uploading stuff (easier with Maven)  #
 #                                                  #
 ####################################################
-
+ARTIFACT_ID=$(cat src/keycloak/meta.json | jq -r '."module-name"')
+ARTIFACT_VERSION=$(cat src/keycloak/meta.json | jq -r '."version"')
+ARTIFACT_TAG=$(cat src/keycloak/meta.json | jq -r '."tag"')
+JAR_FILENAME="$ARTIFACT_ID-$ARTIFACT_VERSION.jar"
 
 if [[ $ARTIFACT_TAG =~ v(.+) ]]; then
     newversion=${BASH_REMATCH[1]}
