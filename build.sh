c41c1fe7be6e5c75415ab0d023fd3dd409c6378d
jeremiegrodziski <jeremie@grodziski.com>
2021-04-21 13:06:20 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ JAR_FILENAME="$ARTIFACT_ID-$ARTIFACT_VERSION.jar"
 
 echo -e "Build \"keycloak-clojure\" jar: target/$JAR_FILENAME"
 
-clj -M:build --app-group-id keycloak-clojure --app-artifact-id $ARTIFACT_ID --app-version $ARTIFACT_VERSION 2>&1 > /dev/null
+clj -X:jar :group-id keycloak-clojure :artifact-id $ARTIFACT_ID :version $ARTIFACT_VERSION :jar target/$JAR_FILENAME 2>&1
 
 if [ $? -eq 0 ]; then
     echo "Successfully built \"keycloak-clojure\"'s artifact: target/$JAR_FILENAME"
