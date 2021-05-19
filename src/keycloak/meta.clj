9122f045d55b2bb61eb208ca1076486fdd72d93f
jeremiegrodziski <jeremie@grodziski.com>
2021-05-19 16:39:09 +0200

    Bump keycloak-clojure to 1.16.1 and spit metadata

--- src/keycloak/meta.clj
+++ src/keycloak/meta.clj
@@ -3,6 +3,6 @@
 
 (def module-name "keycloak-clojure")
 (def path ".")
-(def version "1.16.0")
-(def tag "v1.16.0")
-(def generated-at "2021-04-28T13:56:18Z")
+(def version "1.16.1")
+(def tag "v1.16.1")
+(def generated-at "2021-05-19T14:39:09Z")
