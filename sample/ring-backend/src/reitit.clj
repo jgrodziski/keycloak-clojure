001cdda7e17674e210e4bb57e59f59fb9e11702a
jeremiegrodziski <jeremie@grodziski.com>
2021-06-05 09:05:37 +0200

    Add some utility functions for access control

--- sample/ring-backend/src/reitit.clj
+++ sample/ring-backend/src/reitit.clj
@@ -15,3 +15,25 @@
 (defn reitit-extract-roles [router]
   (memoize (fn [method uri] (get-in (rc/match-by-path router uri) [:data method :access-control :roles]))))
 
+(defn ->access-control [allow-origin]
+  {:scheme            :keycloak
+   :allow-origin      allow-origin
+   :allow-credentials true
+   :allow-methods     #{:get :post :options :delete :put}
+   :allow-headers     #{"Content-Type" "Access-Control-Allow-Headers" "Access-Control-Allow-Origin" "Authorization" "X-Requested-With" "X-Authorization-Token" "Access-Control-Request-Method" "x-request-id"}
+   :expose-headers    #{"Content-Type" "Access-Control-Allow-Headers" "Access-Control-Allow-Origin" "Authorization" "X-Requested-With" "X-Authorization-Token" "Access-Control-Request-Method"}})
+
+(defn request [allow-origin authorized_roles handler]
+  {:produces       ["application/json" "application/transit+json" "application/edn"]
+   :consumes       ["application/json" "application/transit+json" "application/edn"]
+   :access-control {:roles authorized_roles :headers (->access-control allow-origin)}
+   :handler handler})
+
+
+(defn options_request [allow-origin type]
+  {:produces       ["application/json" "application/transit+json" "application/edn"]
+   :consumes       ["application/json" "application/transit+json" "application/edn" "*/*"]
+   :access-control {:roles [] :headers (->access-control allow-origin)}
+   :handler (fn [req]
+              (let [query (->query (ring-extract-account-data req) type)]
+                {:body {} :status  200}))})
