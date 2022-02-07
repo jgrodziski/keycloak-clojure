88a0c227d845d81a82157037116b80b657e75cc1
Jérémie Grodziski <jeremie@grodziski.com>
2022-02-07 12:50:37 +0100

    Fix release script

--- release.sh
+++ release.sh
@@ -30,7 +30,6 @@ ARTIFACT_VERSION=$(cat src/keycloak/meta.json | jq -r '."version"')
 ARTIFACT_TAG=$(cat src/keycloak/meta.json | jq -r '."tag"')
 JAR_FILENAME="$ARTIFACT_ID-$ARTIFACT_VERSION.jar"
 
-clj -X:deploy :artifact \"$(echo target/$JAR_FILENAME)\"
 
 if [ $? -eq 0 ]; then
     echo "Successfully deployed \"$MODULE_NAME\" version $newversion to clojars"
@@ -55,3 +54,6 @@ else
     echo "Fail to push docker image to docker hub!"
     exit $?
 fi
+
+# deploy to clojar after docker buildx because asking the gpg passphrase cana be hidden with docker buildx...:-(
+clj -X:deploy :artifact \"$(echo target/$JAR_FILENAME)\"
