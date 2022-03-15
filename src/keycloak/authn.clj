3f0e4912e78c21fa2dcebcea96eedb8ad0d4a281
Jérémie Grodziski <jeremie@grodziski.com>
2022-03-15 12:31:40 +0100

    Add token store feature and near-expiration? predicate in  ns

--- src/keycloak/authn.clj
+++ src/keycloak/authn.clj
@@ -23,7 +23,8 @@
 
 (defn authenticate
   "Return the bearer token decoded as a clojure map with `:access_token` and `:refresh_token` keys, beware underscore `_` not hyphen `-`.
-  The keycloak conf needs the `:auth-server-url`, `realm` and `client-id` keys"
+   Also contains the `:expires_in` and `:refresh_expires_in` values of token duration in seconds.
+   The keycloak conf needs the `:auth-server-url`, `realm` and `client-id` keys"
   ([{:keys [auth-server-url realm client-id] :as conf} client-secret]
    (authenticate auth-server-url realm client-id client-secret))
   ([{:keys [auth-server-url realm client-id] :as conf} username password]
@@ -52,3 +53,27 @@
   "Return a map with \"authorization\" key and value the access token with \"Bearer \" prefix. Argument is the data structure returned by the `keycloak.authn/authenticate` function"
   [bearer]
   {"authorization" (str "Bearer " (:access_token bearer))})
+
+(defn near-expiration?
+  "Does the token from the `token-store` map expires in less than 20 seconds?"
+  [token-store username]
+  (when (contains? token-store username)
+    (let [elapsed-time (when (get-in token-store [username :issued-at])
+                         (- (System/currentTimeMillis) (get-in token-store [username :issued-at])))]
+      ;(println (get-in token-store [username :issued-at]) (System/currentTimeMillis) (get-in token-store [username :expires_in]))
+      (when-let [now-expires-in (- (* 1000 (get-in token-store [username :expires_in])) elapsed-time)]
+        ;(println "expired in " now-expires-in)
+        (< now-expires-in 20000)))))
+
+(defn token!
+  "Given a map in an atom as a token store, issue, cache and re-issue a token from a keycloak with a username and password.
+   Swap the map entry with the `username` key with this function result (actually the same response as the `keycloak.authn/authenticate` function)"
+  [token-store keycloak-conf username password]
+  (let [exists?          (contains? @token-store username)
+        near-expiration? (near-expiration? @token-store username)]
+    ;(println keycloak-conf username password exists? near-expiration? @token-store)
+    (if (and exists? (not near-expiration?))
+      (get @token-store username)
+      (let [authn-result (assoc (authenticate keycloak-conf username password) :issued-at (System/currentTimeMillis))]
+        (swap! token-store assoc username authn-result)
+        authn-result))))
