bafc57dd2ef43f4a75aa7819100a0b04eebe6055
Jérémie Grodziski <jeremie@grodziski.com>
2022-05-11 16:13:33 +0200

    Fix release script

--- release.sh
+++ release.sh
@@ -30,30 +30,5 @@ ARTIFACT_VERSION=$(cat src/keycloak/meta.json | jq -r '."version"')
 ARTIFACT_TAG=$(cat src/keycloak/meta.json | jq -r '."tag"')
 JAR_FILENAME="$ARTIFACT_ID-$ARTIFACT_VERSION.jar"
 
-
-if [ $? -eq 0 ]; then
-    echo "Successfully deployed \"$MODULE_NAME\" version $newversion to clojars"
-else
-    echo "Fail to deploy \"$MODULE_NAME\" to clojars!"
-    exit $?
-fi
-
-docker buildx build --platform linux/amd64,linux/arm64 -t jgrodziski/keycloak-clojure-starter:latest --push .
-
-if [ $? -eq 0 ]; then
-    echo "Successfully pushed jgrodziski/keycloak-clojure-starter:latest to docker hub"
-else
-    echo "Fail to push docker image to docker hub!"
-    exit $?
-fi
-
-docker buildx build --platform linux/amd64,linux/arm64 -t jgrodziski/keycloak-clojure-starter:$ARTIFACT_VERSION --push .
-if [ $? -eq 0 ]; then
-    echo "Successfully pushed jgrodziski/keycloak-clojure-starter:$ARTIFACT_VERSION to docker hub"
-else
-    echo "Fail to push docker image to docker hub!"
-    exit $?
-fi
-
 # deploy to clojar after docker buildx because asking the gpg passphrase cana be hidden with docker buildx...:-(
 clj -X:deploy :artifact \"$(echo target/$JAR_FILENAME)\"
