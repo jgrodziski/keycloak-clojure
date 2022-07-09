552fe1eba047f99155dfe5307e5c30ee97a52bf2
Jérémie Grodziski <jeremie@grodziski.com>
2022-07-09 12:58:12 +0200

    Bump keycloak-clojure to 1.28.4 and spit metadata

--- src/keycloak/meta.clj
+++ src/keycloak/meta.clj
@@ -3,6 +3,6 @@
 
 (def module-name "keycloak-clojure")
 (def path ".")
-(def version "1.28.3")
-(def tag "v1.28.3")
-(def generated-at "2022-06-22T10:58:59Z")
+(def version "1.28.4")
+(def tag "v1.28.4")
+(def generated-at "2022-07-09T10:58:12Z")
